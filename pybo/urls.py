f345aa171374fcc72411215477f70f89969f1cb8
fnzl54 <rnjscksdud1@gmail.com>
2023-01-03 22:32:27 +0900

    feat : URL 별칭 사용

--- pybo/urls.py
+++ pybo/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'pybo'
+
 urlpatterns = [
-    path('', views.index),
-    path('<int:question_id>/', views.detail),
+    # name를 통해 index 별칭부여
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
 ]
